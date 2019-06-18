e2749e518aa18b973e5b3fbde1fd1b573d04f0f7
rboddy <avidryanb@gmail.com>
2019-06-18 08:24:23 -0400

    Styled the metadata a little bit and got a rough idea of how I want to toggle the skull active. Currently having issues getting the mouseover event work after a skull is activated

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const skullDisplay = document.getElementById("skullDisplay");
 const skullTitle = document.getElementById("skullTitle");
 const skullDesc = document.getElementById("skullDesc");
 const skullMulti = document.getElementById("skullMulti");
+const skullDetails = document.getElementById("details");
 
 for (var i = 0; i < skullBox.length; i++) {
   skullBox[i].addEventListener("mouseenter", () => {
     mouseOverSkull(event.target.innerHTML);
+  });
+  skullBox[i].addEventListener("click", () => {
+    clickOnSkull(event.target.innerHTML, event.target);
   });
 }
 
@@ -81,14 +85,21 @@ const skullMetadata = [
 function mouseOverSkull(skullImage) {
   const skullFile = skullImage.replace('<img src="content/SkullImages/', "");
   const skullName = skullFile.replace('.png">', "");
-  console.log(skullName.trim());
   const skullInfo = skullMetadata.filter(
     skull => skull.name.trim().replace(/\s/g, "") === skullName.trim()
   );
-  console.log(skullInfo);
   skullTitle.innerText = skullInfo[0].name;
   skullDesc.innerText = skullInfo[0].desc;
   skullMulti.innerText = `Skull Multiplier: ${skullInfo[0].multiplier}`;
 
   skullDisplay.innerHTML = skullImage;
+  skullDetails.style.display = "block";
+}
+
+function clickOnSkull(skullImage, skullItem) {
+  const activeSkull = skullImage.replace('.png">', 'Active.png">');
+  console.log(activeSkull);
+  console.log(skullItem);
+  skullItem.innerHTML = activeSkull;
+  skullDisplay.innerHTML = activeSkull;
 }
